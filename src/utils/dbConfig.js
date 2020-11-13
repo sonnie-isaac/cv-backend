db0463d387d0702e755bcf39c4139d24d791cc91
Sunny Cletus <scletus@gmail.com>
2020-11-13 12:36:31 +0100

    changes made

--- src/utils/dbConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  dialect: "postgres",
-  username: "jaconnectdb",
-  password: "KTMR1xkldwOTAebjb7TY",
-  host: "jaconnect-pgsql-db.cpjiizah1wx3.eu-central-1.rds.amazonaws.com",
-};
